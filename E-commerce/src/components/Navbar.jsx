28fb2f2c8f1ef17274a0ea50b23e22e2e51ddfe1
AmitxParmar <amitparmar901@gmail.com>
2023-02-25 16:17:16 +0530

    doing navbar, setup react-router

---
+++ E-commerce/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className='nav-center'>
+            <div className="nav-header">
+                {theme === "light-theme" ? (
+                    <Link to='/' 
+                    
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
