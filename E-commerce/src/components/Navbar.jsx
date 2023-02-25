45bb80ae15c04751cfc8bba5a45982d4c384a521
AmitxParmar <amitparmar901@gmail.com>
2023-02-25 19:32:43 +0530

    finish navbar, remove icons from constants

--- E-commerce/src/components/Navbar.jsx
+++ E-commerce/src/components/Navbar.jsx
@@ -35,8 +35,7 @@ const Navbar = () => {
                     </button>
                 </div>
                 <ul className='nav-links'>
-                    {links.map((link) => {
-                        const { id, url, text } = link;
+                    {links.map(({ id, url, text }) => {
                         console.log(id, url, text);
                         return (
                             <li key={id}>
@@ -97,10 +96,36 @@ const NavContainer = styled.nav`
     .cart-btn-wrapper {
         display: none;
     }
-
-
-
-    
-    `;
+@media (min-width: 992px) {
+    .nav-toggle {
+        display: none;
+    } 
+    .nav-center {
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        align-items: center;
+    }
+    .nav-links {
+        display: flex;
+        justify-content:center;
+        li {
+            margin: 0 0.5rem;
+        }
+        a {
+            color: var(--clr-grey-3);
+            font-size: 1rem;
+            text-transform: capitalize;
+            letter-spacing: var(--spacing);
+            padding: 0.5rem;
+            &:hover {
+                border-bottom: 2px solid var(--clr-primary-7);
+            }
+        }
+    }
+    .cart-btn-wrapper {
+        display: grid;
+    }
+}
+`;
 
 export default Navbar
--- E-commerce/src/utils/constants.js
+++ E-commerce/src/utils/constants.js
@@ -21,21 +21,21 @@ export const links = [
 export const services = [
     {
         id: 1,
-        icon: 'mission'/* <GiCompass /> */,
+        icon: 'missionLogo'/* <GiCompass /> */,
         title: 'mission',
         text:
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
     },
     {
         id: 2,
-        icon: "vision", /* < GiDiamondHard /> */
+        icon: "visionLogo", /* < GiDiamondHard /> */
         title: 'vision',
         text:
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
     },
     {
         id: 3,
-        icon: "history"/* <GiStabbedNote /> */,
+        icon: "historyLogo"/* <GiStabbedNote /> */,
         title: 'history',
         text:
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
